99412d03ab75f197e7a92a45c5d980698affd793
rodrigolazarte <romalaz94@gmail.com>
2020-02-28 10:24:36 -0300

    added sign in component and styles

---
+++ src/pages/sign-page/sign-page.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-page.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignPage = () => (
+    <div className='sign-page'>
+        <SignIn />
+    </div>
+)
+
+export default SignPage;
